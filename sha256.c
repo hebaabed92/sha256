e998417fbbc168c669c5d2d4952da7f95cddc20b
Everett Hildenbrandt <ehildenb@mines.edu>
2013-11-12 18:35:35 +0100

    Input/padding working-ish

--- sha256.c
+++ sha256.c
@@ -12,9 +12,10 @@ int main(int argc, char *argv[])
             char message[(512-64)/8];
             uint64_t length;
         } info;
-        uint8_t test[512/8];
+        //uint8_t test[512/8];
     } input;
     int msgSize = 0;
+    int i = 0;
     
     if(argc > 1) {
         printf("Input from stdin, not command line (for security reasons?)\n");
@@ -29,11 +30,17 @@ int main(int argc, char *argv[])
     if(msgSize > ((512-64)/8)-1)
         msgSize = ((512-64)/8-1);
     input.all[msgSize] = 1 << 7; // I think this is ok for just making the left-most bit a 1? 
-    for(msgSize += 1; msgSize < 512/8; msgSize ++)
-        input.all[msgSize] = 0 << 7;
-    int test = 0;
-    printf("Output now:\n");
-    for(test = 0; test < (512/8)-1; test ++)
-        printf("\t%d", input.test[test]);
+    for(i = msgSize + 1; i < 512/8; i ++)
+        input.all[i] = 0 << 7;
+    input.info.length = msgSize*8;
+    
+
+    /*int test = 0;
+    printf("Output now:%s\n", input.all);
+    for(test = 0; test < (512/8); test ++)
+        printf("%d\t", input.test[test]);
+    printf("\nStored Message size: %d\n", input.info.length);*/
+
+
     return 0;
 }
