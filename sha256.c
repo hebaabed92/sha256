367b209b0c33081394d221e41c027f7c3f5c20d3
Everett Hildenbrandt <ehildenb@mines.edu>
2013-11-12 16:50:53 +0100

    Progress

--- sha256.c
+++ sha256.c
@@ -3,21 +3,22 @@
 
 #include <stdio.h>
 #include <stdint.h>
-
+#include <string.h>
 int main(int argc, char *argv[])
 {
-    char input[512];
+    char input[512/8];
+    int msgSize = 0;
     if(argc > 1) {
         printf("Input from stdin, not command line (for security reasons?)\n");
         return 1;
     }
-    if(fgets(input, 512, stdin) == NULL) {
+    if(fgets(input, sizeof(input), stdin) == NULL) {
         printf("Error reading stdin - fgets returned NULL. Returning.\n");
         return 2;
     }
-
     
-
+    msgSize = strlen(input);
+    
     
     return 0;
 }
