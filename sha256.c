829ba52dae54ffdc1256e860a01d2e1309db788b
Everett Hildenbrandt <ehildenb@mines.edu>
2013-11-12 12:24:35 +0100

    Add error check on stdin read

--- sha256.c
+++ sha256.c
@@ -1,8 +1,6 @@
 // Everett Hildenbrandt
 // Attempt to implement SHA256 in C to learn some programming and Cryptography
 
-/* Nothing Here Yet */
-
 #include <stdio.h>
 #include <stdint.h>
 
@@ -13,7 +11,9 @@ int main(int argc, char *argv[])
         printf("Input from stdin, not command line (for security reasons?)\n");
         return 1;
     }
-    read(stdin, input, 256);
-    printf("Input recieved: %s", input);    
+    if(read(stdin, input, 256) > 0)
+        printf("Input recieved: %s", input);    
+    else
+        printf("Error.");
     return 0;
 }
