5f4d084975533b3bc78a9cd2ace3e6ab69cf2c5e
Everett Hildenbrandt <ehildenb@mines.edu>
2013-11-12 12:13:42 +0100

    Add standard input

--- sha256.c
+++ sha256.c
@@ -2,3 +2,18 @@
 // Attempt to implement SHA256 in C to learn some programming and Cryptography
 
 /* Nothing Here Yet */
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[])
+{
+    char input[256];
+    if(argc > 1) {
+        printf("Input from stdin, not command line (for security reasons?)\n");
+        return 1;
+    }
+    read(stdin, input, 256);
+    printf("Input recieved: %s", input);    
+    return 0;
+}
