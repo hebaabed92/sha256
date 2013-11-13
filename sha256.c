845d5da69dc96b45dadd48708539aa9cfbdff614
Everett Hildenbrandt <ehildenb@mines.edu>
2013-11-13 14:33:29 +0100

    Initializing K values

--- sha256.c
+++ sha256.c
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include <string.h>
 
-//Macro definitions of initial hash values
+//Macro definitions of constants
 #define H00 0x6a09e667
 #define H01 0xbb67ae85
 #define H02 0x3c6ef372
@@ -14,6 +14,7 @@
 #define H05 0x9b05688c
 #define H06 0x1f83d9ab
 #define H07 0x5be0cd19
+#define KVALS {0x428a2f98, 0xd807aa98, 0xe49b69c1, 0x983e5152, 0x27b70a85, 0xa2bfe8a1, 0x19a4c116, 0x748f82ee, 0x71374491, 0x12835b01, 0xefbe4786, 0xa831c66d, 0x2e1b2138, 0xa81a664b, 0x1e376c08, 0x78a5636f, 0xb5c0fbcf, 0x243185be, 0x0fc19dc6, 0xb00327c8, 0x4d2c6dfc, 0xc24b8b70, 0x2748774c, 0x84c87814, 0xe9b5dba5, 0x550c7dc3, 0x240ca1cc, 0xbf597fc7, 0x53380d13, 0xc76c51a3, 0x34b0bcb5, 0x8cc70208, 0x3956c25b, 0x72be5d74, 0x2de92c6f, 0xc6e00bf3, 0x650a7354, 0xd192e819, 0x391c0cb3, 0x90befffa, 0x59f111f1, 0x80deb1fe, 0x4a7484aa, 0xd5a79147, 0x766a0abb, 0xd6990624, 0x4ed8aa4a, 0xa4506ceb, 0x923f82a4, 0x9bdc06a7, 0x5cb0a9dc, 0x06ca6351, 0x81c2c92e, 0xf40e3585, 0x5b9cca4f, 0xbef9a3f7, 0xab1c5ed5, 0xc19bf174, 0x76f988da, 0x14292967, 0x92722c85, 0x106aa070, 0x682e6ff3, 0xc67178f2} 
 
 //Macro definitions of functions we'll need for hashing
 #define Ch(x, y, z) ((x&y)^((~x)&z))
@@ -42,8 +43,9 @@ int main(int argc, char *argv[])
     int msgSize = 0;
     int i = 0;
     uint32_t a, b, c, d, e, f, g, h;
-    uint32_t T1, T2, K;
+    uint32_t T1, T2;
     uint32_t W[64];
+    uint32_t K[64] = KVALS;
     
     if(argc > 1) {
         printf("Input from stdin, not command line (for security reasons?)\n");
@@ -92,7 +94,7 @@ int main(int argc, char *argv[])
     //SHA-256 compression function loop
     for(i=0; i<64; i++)
     {
-        T1 = h + SIG1(e) + Ch(e, f, g) + K + W;
+        T1 = h + SIG1(e) + Ch(e, f, g) + K[i] + W[i];
         T2 = SIG0(a) + Maj(a, b, c);
         h = g;
         g = f;
