60b361de44cbffb033d8f4bcb8c76d8d206df7a1
Everett Hildenbrandt <ehildenb@mines.edu>
2013-11-15 13:53:44 +0100

    Working hash! Endian problem on output

--- sha256.c
+++ sha256.c
@@ -8,7 +8,7 @@
 
 //Macro definitions of constants
 #define HVALS {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19}
-#define KVALS {0x428a2f98, 0xd807aa98, 0xe49b69c1, 0x983e5152, 0x27b70a85, 0xa2bfe8a1, 0x19a4c116, 0x748f82ee, 0x71374491, 0x12835b01, 0xefbe4786, 0xa831c66d, 0x2e1b2138, 0xa81a664b, 0x1e376c08, 0x78a5636f, 0xb5c0fbcf, 0x243185be, 0x0fc19dc6, 0xb00327c8, 0x4d2c6dfc, 0xc24b8b70, 0x2748774c, 0x84c87814, 0xe9b5dba5, 0x550c7dc3, 0x240ca1cc, 0xbf597fc7, 0x53380d13, 0xc76c51a3, 0x34b0bcb5, 0x8cc70208, 0x3956c25b, 0x72be5d74, 0x2de92c6f, 0xc6e00bf3, 0x650a7354, 0xd192e819, 0x391c0cb3, 0x90befffa, 0x59f111f1, 0x80deb1fe, 0x4a7484aa, 0xd5a79147, 0x766a0abb, 0xd6990624, 0x4ed8aa4a, 0xa4506ceb, 0x923f82a4, 0x9bdc06a7, 0x5cb0a9dc, 0x06ca6351, 0x81c2c92e, 0xf40e3585, 0x5b9cca4f, 0xbef9a3f7, 0xab1c5ed5, 0xc19bf174, 0x76f988da, 0x14292967, 0x92722c85, 0x106aa070, 0x682e6ff3, 0xc67178f2} 
+#define KVALS {0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 }
 
 //Macro definitions of functions we'll need for hashing
 #define Ch(x, y, z) ((x&y)^((~x)&z))
@@ -39,9 +39,9 @@ int main(int argc, char *argv[])
     uint32_t W[64];
     uint32_t K[64] = KVALS;
     uint32_t H[8] = HVALS;
-    uint32_t x = 1;
-    char *y = (char*)&x;
-    printf("LittleBig: %c\n\n", *y+48);
+    //uint32_t x = 1;
+    //char *y = (char*)&x;
+    //printf("LittleBig: %c\n\n", *y+48);
    
     if(argc > 1) {
         printf("Input from stdin, not command line (for security reasons?)\n");
@@ -51,17 +51,7 @@ int main(int argc, char *argv[])
         printf("Error reading stdin - fgets returned NULL. Returning.\n");
         return 2;
     }
-    for(i = 0; i < 512/32; i++)
-        input.mi[i] = input.mi[i];
-    printf("Every character stored:\n");
-    for(i = 0; i < (512/8); i++)
-        printf("%c", input.all[i]);
-    printf("\n");
-    printf("Initial M Values:\n");
-    for(i = 0; i < 16; i++)
-        printf("%08x ", input.mi[i]);
-    printf("\n\n");
-    
+
     //Pre-processing - truncate the message (we are only using 1 block)
     //Pad the message according to the SHA256 specification
     //Print the size of the message in bits at the end of the block
@@ -70,10 +60,11 @@ int main(int argc, char *argv[])
         msgSize = ((512-64)/8-1);
     for(i = msgSize; i < 512/8; i++)
         input.all[i] = (0<<7);
-    input.all[msgSize] = (1<<7); // I think this is ok for just making the left-most bit a 1? 
+    input.all[msgSize] = (1<<7);
     input.info.length = bswap_64(msgSize*8);
-    for(i = 0; i < (512/32)-2; i++)
+    for(i = 0; i < (512/32); i++)
         input.mi[i] = bswap_32(input.mi[i]);
+   
     /*printf("Every character stored:\n");
     for(i = 0; i < (512/8); i++)
         printf("%c", input.all[i]);
@@ -83,24 +74,12 @@ int main(int argc, char *argv[])
         printf("%08x ", input.mi[i]);
     printf("\n\n");*/
     
-    printf("Every character stored:\n");
-    for(i = 0; i < (512/8); i++)
-        printf("%c", input.all[i]);
-    printf("\n");
-    printf("Initial M Values:\n");
+    printf("Initial Message Block (W):\n");
     for(i = 0; i < 16; i++)
         printf("%08x ", input.mi[i]);
     printf("\n");
  
-
-    //Block for testing whether the pre-processing went correctly
-    /*int test = 0;
-    printf("Output now:%s\n", input.all);
-    for(test = 0; test < (512/8); test ++)
-        printf("%d\t", input.test[test]);
-    printf("\nStored Message size: %d\n", input.info.length);*/
-    
-    //Initializing the values ("registers")
+    //Initializing the values ("registers" - maybe, who knows)
     a = H[0];
     b = H[1];
     c = H[2];
@@ -114,7 +93,7 @@ int main(int argc, char *argv[])
         W[i] = input.mi[i]; 
     for(i=16; i<64; i++)
         W[i] = sig1(W[i-2])+W[i-7]+sig0(W[i-15])+W[i-16];
- 
+ printf("\nt=%d\t%08x %08x %08x %08x %08x %08x %08x %08x", i, a, b, c, d, e, f, g, h);
     //SHA-256 compression function loop
     for(i=0; i<64; i++)
     {
@@ -128,6 +107,7 @@ int main(int argc, char *argv[])
         c = b;
         b = a;
         a = T1 + T2;
+        printf("\nt=%d\t%08x %08x %08x %08x %08x %08x %08x %08x", i, a, b, c, d, e, f, g, h);
     }
 	H[0] = a + H[0];
 	H[1] = b + H[1];
@@ -137,7 +117,8 @@ int main(int argc, char *argv[])
 	H[5] = f + H[5];
 	H[6] = g + H[6];
 	H[7] = h + H[7];
-    
+
+    printf("\n\n");
     for(i=0; i<8; i++)
         printf("%x ", bswap_32(H[i]));
     printf("\n");
